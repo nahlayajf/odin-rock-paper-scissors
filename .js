8bc652d5b2577611d29ddb5005df07d19b0ed938
nahlayajf <06639425+nahlayajf@users.noreply.github.com.>
2026-05-11 17:46:52 -0400

    Make computer generate a random choice
    Computer returns a random choice out of rock, paper, or scissors.

---
+++ .js
@@ -0,0 +1,16 @@
+// Computer randomly returns rock, paper, or scissors
+function getComputerChoice() {
+    let choiceNum = Math.floor(Math.random() * 3)
+
+    if (choiceNum === 1) {
+        return "rock";
+    }
+    else if (choiceNum === 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+
+}
+
